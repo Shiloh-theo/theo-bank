17204a877fa4d71a32a04b7aa2a819827bf35031
shiloh theophilus <shilohtheophilus45@gmail.com>
2025-05-15 13:07:07 +0200

    initial commit for bank app with all microservices

---
+++ authentication/src/test/java/theo/bank/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package theo.bank.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
